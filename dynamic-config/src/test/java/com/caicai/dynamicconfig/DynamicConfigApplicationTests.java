234e2ce333f58d9d80dc8ae95915b64b07d14d27
caicai <yujie.wan@freemud.com>
2021-01-08 18:22:08 +0800

    DynamicInit

---
+++ dynamic-config/src/test/java/com/caicai/dynamicconfig/DynamicConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caicai.dynamicconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+[toc]
+## jdk-demo
+- 日常java测试代码
+## log
+- slf4j日志
+## spring-boot
+
